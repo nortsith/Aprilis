3913d9f653b6bde0353d528899bd784f14bfda2c
Onur Sahin Senturk <Onur Sahin Senturk>
2017-08-30 00:30:59 +0300

    Add new game and game information stuff.

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,7 +2,13 @@
   var online_users = jQuery('#users'),
       users_frame  = jQuery('.frame'),
       search_input = jQuery('#search'),
-      game_element = jQuery('.game');
+      game_element = jQuery('.game'),
+      aprilis_logo = jQuery('.logo'),
+      content      = jQuery('.content'),
+      game_content = jQuery('.game_content'),
+      add_button   = jQuery('.add_button'),
+      add_game     = jQuery('.add_game'),
+      blur         = jQuery('.blur');
 
   index.init = function(){
     index.updates();
@@ -15,9 +21,48 @@
 
   index.events = function(){
     search_input.keyup(function(){
+      game_content.removeClass('active');
       index.search_query();
     });
+
+    content.on('scroll',function(){
+      game_content.css('top',content.scrollTop()+'px');
+    });
+
+    jQuery(document).on('click','.game',function(){
+        game_content.addClass('active');
+        jQuery('.content').addClass('scroll_disable');
+    });
+
+    game_content.on('click',function(){
+      index.enable_scroll();
+    });
+
+    aprilis_logo.on('click',function(){
+      index.enable_scroll();
+    });
+
+    add_button.on('click',function(){
+      blur.fadeIn();
+      setTimeout(function(){
+        add_game.fadeIn();
+      },500);
+    });
+
+    blur.on('click',function(){
+      add_game.fadeOut();
+      setTimeout(function(){
+        blur.fadeOut();
+      },500);
+    });
   };
+
+  index.enable_scroll = function(){
+    game_content.removeClass('active');
+    setTimeout(function(){
+      content.removeClass('scroll_disable');
+    },1000);
+  }
 
   index.search_query = function(){
     jQuery('.game').each(function(index){
--- public/javascript/server_communication.js
+++ public/javascript/server_communication.js
@@ -2,7 +2,10 @@
   var socket = io();
   var online_users  = jQuery('#users'),
       users_frame   = jQuery('.frame'),
-      games_wrapper = jQuery('#games_container');
+      games_wrapper = jQuery('#games_container'),
+      add_button    = jQuery('#add_button'),
+      add_game     = jQuery('.add_game'),
+      blur         = jQuery('.blur');
 
   client.init = function(){
     socket.connect();
@@ -21,12 +24,72 @@
     });
   };
 
-  client.add_game = function(data){
-    jQuery.ajax({
-      url: "/games?data="+JSON.stringify(data),
-      type: 'POST',
-    });
-    socket.emit('new_game_added',data);
+  client.add_game = function(){
+    var inputs = [
+      title = {
+        element:  jQuery('#data_title'),
+        value  :  jQuery('#data_title').val()
+      },
+      dungeon_master = {
+        element:  jQuery('#data_dungeon_master'),
+        value  :  jQuery('#data_dungeon_master').val()
+      },
+      player_limit = {
+        element:  jQuery('#data_player_limit'),
+        value  :  jQuery('#data_player_limit').val()
+      },
+      schedule ={
+        element:  jQuery('#data_schedule'),
+        value  :  jQuery('#data_schedule').val()
+      },
+      player_level = {
+        element:  jQuery('#data_player_level'),
+        value  :  jQuery('#data_player_level').val()
+      },
+      system = {
+        element:  jQuery('#data_system'),
+        value  :  jQuery('#data_system').val()
+      },
+      description = {
+        element:  jQuery('#data_description'),
+        value  :  jQuery('#data_description').val()
+      },
+    ];
+
+    if(inputs[0].value && inputs[1].value && inputs[2].value && inputs[3].value && inputs[4].value && inputs[5].value && inputs[6].value){
+      inputs.forEach(function(input,index){
+        input.element.removeClass("warning");
+      });
+
+      var data = {
+        title          :inputs[0].value,
+        dungeon_master :inputs[1].value,
+        player_limit   :inputs[2].value,
+        schedule       :inputs[3].value,
+        player_level   :inputs[4].value,
+        system         :inputs[5].value,
+        description    :inputs[6].value,
+        player_list    : []
+      };
+
+      add_game.fadeOut();
+
+      jQuery.ajax({
+        url: "/games?data="+JSON.stringify(data),
+        type: 'POST',
+        success: function(){
+          setTimeout(function(){
+            blur.fadeOut();
+          },500);
+          socket.emit('new_game_added',data);
+        }
+      });
+
+    } else{
+      inputs.forEach(function(input,index){
+        input.value.length <= 0 ? input.element.addClass("warning") : input.element.removeClass("warning");
+      });
+    }
   };
 
   client.get_data = function(){
@@ -36,8 +99,8 @@
       player_limit:jQuery('#data_player_limit'),
       schedule:jQuery('#data_schedule'),
       player_level:jQuery('#data_player_level'),
-      description:jQuery('#data_description'),
       system:jQuery('#data_system'),
+      description:jQuery('#data_description'),
       player_list: []
     }
   }
@@ -57,8 +120,16 @@
 
   client.events = function(){
     socket.on('add_new_game',function(data){
-      var game = client.generate_game_html(data);
-      games_wrapper.append(game);
+      jQuery('.game').remove();
+      client.get_games();
+    });
+
+    jQuery(document).on('click','.game',function(){
+        client.update_game_content(this);
+    });
+
+    add_button.on('click',function(){
+      client.add_game();
     });
     // socket.on('user_connected',function(user_count){
     //   client.update_user_count(user_count);
@@ -66,6 +137,36 @@
     // socket.on('user_disconnected',function(user_count){
     //   client.update_user_count(user_count);
     // });
+  };
+
+  client.update_game_content = function(element){
+    var title          = jQuery('#game_title'),
+        dungeon_master = jQuery('#dungeon_master'),
+        player_limit   = jQuery('#player_limit'),
+        schedule       = jQuery('#schedule'),
+        player_level   = jQuery('#player_level'),
+        system         = jQuery('#system'),
+        description    = jQuery('#description'),
+        game_id = jQuery(element).attr('data-id');
+
+    jQuery.ajax({
+      url: "/games?type=get",
+      type: 'POST',
+      success: function(games){
+        for(var id in games){
+          if(game_id == JSON.parse(games[id]).id){
+            var game_data = JSON.parse(games[id]);
+            title.text(game_data.title);
+            dungeon_master.text(game_data.dungeon_master);
+            player_limit.text(game_data.player_limit);
+            schedule.text(game_data.schedule);
+            player_level.text(game_data.player_level);
+            system.text(game_data.system);
+            description.text(game_data.description);
+          }
+        }
+      }
+    });
   };
 
   client.update_user_count = function(user_count){
@@ -88,6 +189,8 @@
       config.element.removeClass(config.class);
     },config.delay || 500);
   };
+
+  client.add_game
 
   client.init();
 })({});
